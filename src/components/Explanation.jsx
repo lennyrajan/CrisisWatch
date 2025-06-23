d72a9dd567cdbb2e41bf65fdbe4ff265cac26d94
lennyrajan <lennyrajan@gmail.com>
2025-06-23 10:27:33 -0700

    Update Explanation.jsx

--- src/components/Explanation.jsx
+++ src/components/Explanation.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
+export default function Explanation({ markdown }) {
+  if (!markdown) return <p>Loading explanation...</p>;
+
+  return (
+    <section style={{ marginTop: 30 }}>
+      <h2>What’s Happening?</h2>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </section>
+  );
+}
