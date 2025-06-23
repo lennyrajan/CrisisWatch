be4fa47b0b8d3081f58df97ecba804c2f5524c5c
lennyrajan <lennyrajan@gmail.com>
2025-06-23 10:28:21 -0700

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,1 +1,21 @@
+import React from "react";
 
+export default function Timeline({ events }) {
+  if (!events.length) return <p>Loading timeline...</p>;
+
+  return (
+    <section style={{ marginTop: 30 }}>
+      <h2>Timeline of Events</h2>
+      <ul>
+        {events.map(({ date, title, description }, i) => (
+          <li key={i} style={{ marginBottom: 15 }}>
+            <strong>{new Date(date).toLocaleString()}</strong>
+            <br />
+            <em>{title}</em>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
